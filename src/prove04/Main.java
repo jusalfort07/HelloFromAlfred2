3cdea84ce05500ef2541b0acc9553ea01dd6581f
Alfred Ortega <jusalfort07@byui.edu>
2021-01-29 13:41:41 +0800

    Initial Commit

---
+++ src/prove04/Main.java
@@ -0,0 +1,9 @@
+package prove04;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String text = "Hello! My name is Alfred";
+        System.out.println(text);
+    }
+}
