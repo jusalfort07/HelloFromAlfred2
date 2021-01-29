584b116aaa74ea6054bd7403a32627268132958a
Michael <sie20003@byui.edu>
2021-01-29 17:29:38 +1100

    Added reply as instructed on step 07.

--- src/prove04/Main.java
+++ src/prove04/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
         String text = "Hello! My name is Alfred";
         System.out.println(text);
+
+        String text2 = "Hello Alfred, my name is A. Michael Sierra.";
+        System.out.println(text2);
     }
 }
